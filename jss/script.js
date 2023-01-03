f388aafd6fd24bb58635d71e8f20659866958cda
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-03 17:05:41 +0100

    update

---
+++ jss/script.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
+
+
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+const API_KEY = '&appid=67a3e85e409e0b539d55bd0f374d6c4b';
+const API_UNITS = '&units=metric';
+
+const getWeather = () => {
+    const city = input.value;
+    const URL = API_URL + city + API_KEY + API_UNITS;
+}
