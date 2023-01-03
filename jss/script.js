4e1b6f50b2b35b7c2127f62af7bfb6c262f81275
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-03 20:55:17 +0100

    update

--- jss/script.js
+++ jss/script.js
@@ -8,11 +8,15 @@ const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
 
 
-const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=67a3e85e409e0b539d55bd0f374d6c4b';
 const API_UNITS = '&units=metric';
 
 const getWeather = () => {
-    const city = input.value;
+    const city = input.value || 'London';
     const URL = API_URL + city + API_KEY + API_UNITS;
+
+    axios.get(URL).then(res =>console.log(res))
 }
+
+getWeather()
