56440fffc4ab087cafbfb7fa9654d2929df1d499
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-03 22:05:41 +0100

    update js

--- js/script.js
+++ js/script.js
@@ -21,11 +21,30 @@ const getWeather = () => {
         const temp = res.data.main.temp
         const hum = res.data.main.humidity
         const status = Object.assign({}, ...res.data.weather)
+
             
         cityName.textContent = res.data.name
         temperature.textContent = Math.floor(temp) + '°C'
         humidity.textContent = hum + '%'
         weather.textContent = status.main
+
+        if (status.id >= 200 && status.id <300) {
+            photo.setAttribute('src', './img/thunderstorm.png')
+        } else if (status.id >= 300 && status.id <= 400) {
+            photo.setAttribute('src', './img/drizzle.png')
+        } else if (status.id >= 500 && status.id <= 531) {
+            photo.setAttribute('src', './img/rain.png')
+        } else if (status.id >= 600 && status.id <= 621) {
+            photo.setAttribute('src', './img/ice.png')
+        } else if (status.id >= 701 && status.id <= 781) {
+            photo.setAttribute('src', './img/fog.png')
+        } else if (status.id === 800) {
+            photo.setAttribute('src', './img/sun.png')
+        } else if (status.id >= 801 && status.id <= 804) {
+            photo.setAttribute('src', './img/cloud.png')
+        } else {
+            photo.setAttribute('src', './img/unknown.png')
+        }
     })
 }
 
